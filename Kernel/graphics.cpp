a39cead03be1b3f86012cb69e99dcb154483055b
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-10-20 22:35:14 +0900

    いくつかの描画関数を追加

--- Kernel/graphics.cpp
+++ Kernel/graphics.cpp
@@ -13,3 +13,23 @@ void BGRResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor& color
   p[1] = color.g;
   p[2] = color.r;
 }
+
+void DrawRectangle(PixelWriter& writer, const Vector2D<int>& pos, const Vector2D<int>& size, const PixelColor& color) {
+  for(int x = 0; x < size.x; x++) {
+    writer.Write(pos.x + x, pos.y             , color);
+    writer.Write(pos.x + x, pos.y + size.y - 1, color);
+  }
+
+  for(int y = 0; y < size.y; y++) {
+    writer.Write(pos.x             , pos.y + y, color);
+    writer.Write(pos.x + size.x - 1, pos.y + y, color);
+  }
+}
+
+void FillRectangle(PixelWriter& writer, const Vector2D<int>& pos, const Vector2D<int>& size, const PixelColor& color) {
+  for(int y = 0; y < size.y; y++) {
+    for(int x = 0; x < size.x; x++) {
+      writer.Write(pos.x + x, pos.y + y, color);
+    }
+  }
+}
