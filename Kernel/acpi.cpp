1b3ff05614e5c9037e0f73db5ce5250aecc4e5e0
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-11-05 10:11:33 +0900

    XSDTおよびFADT構造体の実装

--- Kernel/acpi.cpp
+++ Kernel/acpi.cpp
@@ -26,6 +26,26 @@ void acpi::Initialize(const acpi::RSDP& rsdp) {
     Log(kError, "RSDP is not valid");
     exit(1);
   }
+
+  const acpi::XSDT& xsdt = *reinterpret_cast<XSDT*>(rsdp.xsdt_address);
+  if(!xsdt.header.IsValid("XSDT")) {
+    Log(kError, "XSDT is not valid\n");
+    exit(1);
+  }
+
+  acpi::fadt = nullptr;
+  for(int i = 0; i < xsdt.Count(); i++) {
+    const auto& entry = xsdt[i];
+    if(entry.IsValid("FACP")) {
+      acpi::fadt = reinterpret_cast<const FADT*>(&entry);
+      break;
+    }
+  }
+
+  if(fadt == nullptr) {
+    Log(kError, "FADT is not found\n");
+    exit(1);
+  }
 }
 
 bool acpi::RSDP::IsValid() const {
@@ -50,4 +70,27 @@ bool acpi::RSDP::IsValid() const {
   }
 
   return true;
+}
+
+bool acpi::DescriptionHeader::IsValid(const char* expected_signature) const {
+  if(strncmp(this->signature, expected_signature, 4) != 0) { 
+    Log(kError, "invalid signature: %.4s expected: %.4s\n", this->signature, expected_signature);
+    return false;
+  }
+
+  if(auto sum = SumBytes(this, this->length); sum != 0) {
+    Log(kError, "sum of %u bytes must be 0: %d\n", sum);
+    return false;
+  }
+
+  return true;
+}
+
+const acpi::DescriptionHeader& acpi::XSDT::operator[](size_t i) const {
+  auto entries = reinterpret_cast<const uint64_t*>(&this->header + 1);
+  return *reinterpret_cast<acpi::DescriptionHeader*>(entries[i]);
+}
+
+size_t acpi::XSDT::Count() const {
+  return (this->header.length - sizeof(DescriptionHeader)) / sizeof(uint64_t);
 }
