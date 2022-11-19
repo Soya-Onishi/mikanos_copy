ed1499107b10807a5031e9d62b3ca77134d783dd
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-11-19 14:05:52 +0900

    サンプルプログラムを標準ライブラリを使用するコードに変更 strcmpが予想通りに動いてくれないため、1文字目の比較に変更

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -1,22 +1,5 @@
-int strcmp(const char* a, const char* b) {
-  int i = 0;
-  for(; a[i] != 0 && b[i] != 0; ++i) {
-    if(a[i] != b[i]) {
-      return a[i] - b[i];
-    }
-  }
-
-  return a[i] - b[i];
-}
-
-long atol(const char* s) {
-  long v = 0;
-  for(int i = 0; s[i] != 0; i++) {
-    v = v * 10 + (s[i] - '0');
-  }
-
-  return v;
-}
+#include <cstring>
+#include <cstdlib>
 
 int stack_idx;
 long stack[128];
@@ -33,21 +16,27 @@ void Push(long value) {
 }
 
 extern "C" int main(int argc, char** argv) {
+  int error_count = 0;
+
   // main呼び出し前の段階のBSS領域などの初期化処理が実装されていないため
   // グローバル変数はmain関数で初期化する 
   stack_idx = -1;
 
   for(int i = 1; i < argc; i++) {
-    if(strcmp(argv[i], "+") == 0) {
+    if(argv[i][0] == '+') {
       long b = Pop();
       long a = Pop();
       Push(a + b);
-    } else if (strcmp(argv[i], "-") == 0) {
+    } else if (argv[i][0] == '-') {
       long b = Pop();
       long a = Pop();
       Push(a - b);
     } else {
       long a = atol(argv[i]);
+      if(a == 0) {
+        error_count++;
+      }
+
       Push(a);
     }
 
@@ -56,5 +45,9 @@ extern "C" int main(int argc, char** argv) {
     }
   }
 
-  return static_cast<int>(Pop());
+  if(error_count > 0) {
+    return error_count;
+  } else {
+    return static_cast<int>(Pop());
+  }
 }
